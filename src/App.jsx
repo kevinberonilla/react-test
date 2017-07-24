24798a904b397a76754505b11984d7eeb02a727b
Kevin Beronilla <kevin.beronilla@gmail.com>
2017-07-24 13:12:16 -0500

    Messing around with props and states

--- src/App.jsx
+++ src/App.jsx
@@ -11,17 +11,17 @@ Components
 import Button from './components/button/Button.jsx'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-            <div className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h2>Welcome to React</h2>
+    render() {
+        return (
+            <div className="App">
+               <div className="App-header">
+                   <img src={logo} className="App-logo" alt="logo" />
+                   <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro"><Button title="Button Title" /></p>
             </div>
-            <p className="App-intro">To get started, edit <code>src/App.jsx</code> and save to reload. <span><Button title="Test Button" /></span></p>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
 class Button extends React.Component {
-  render() {
-    return (
-        <button>{this.props.title}</button>
-    );
-  }
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            title: this.props.title || 'No Title Provided'
+        }
+        
+        this.handleOnClick = this.handleOnClick.bind(this);
+    }
+    
+    handleOnClick(e) {
+        console.log('click');
+        console.log(e);
+        
+        this.setState({ title: 'New Button Title' });
+    }
+    
+    render() {
+        return (
+            <button onClick={this.handleOnClick}>{this.state.title}</button>
+        );
+      }
 }
 
 export default Button;
